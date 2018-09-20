101dccb41d5caac8fdf0b9e7b9ecb85070e7d7ad
Tenyo Grozev <tenyo.grozev@yale.edu>
2018-09-20 15:27:53 -0400

    save responses

--- actions/responses.go
+++ actions/responses.go
@@ -2,17 +2,20 @@ package actions
 
 import (
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/YaleSpinup/proctor/libs/helpers"
 	"github.com/YaleSpinup/proctor/models"
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/uuid"
 )
 
 // Outcome has the data types and risk level outcome determined based on the questions response
 type Outcome struct {
-	DataTypes []string `json:"datatypes"`
-	RiskLevel string   `json:"risklevel"`
+	ID        uuid.UUID `json:"id"`
+	DataTypes []string  `json:"datatypes"`
+	RiskLevel string    `json:"risklevel"`
 }
 
 // ResponsesPost processes the question responses and returns the data type and security level in an Outcome
@@ -63,7 +66,23 @@ func ResponsesPost(c buffalo.Context) error {
 	var outcome Outcome
 	outcome.DataTypes = datatypes
 	outcome.RiskLevel = hr.Text
+	outcome.ID, err = uuid.NewV4()
+	if err != nil {
+		return c.Error(500, errors.New("Error generating outcome id"))
+	}
 	log.Println("Outcome response:", outcome)
+
+	// save responses to S3
+	// we also include the original questions and the outcome that was returned to the client
+	s := struct {
+		GeneratedOutcome  Outcome
+		ClientResponse    models.Responses
+		OriginalQuestions models.Questions
+	}{outcome, responses, questions}
+	path := fmt.Sprint(responses.Path(c.Param("campaign")), outcome.ID.String(), ".json")
+	if err := S3.Save(s, path); err != nil {
+		return err
+	}
 
 	return c.Render(200, r.JSON(outcome))
 }
--- libs/s3/objects.go
+++ libs/s3/objects.go
@@ -1,6 +1,7 @@
 package s3
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -56,7 +57,7 @@ func (s Client) ListObjects(prefix, delimiter string) (*s3.ListObjectsOutput, er
 	return result, nil
 }
 
-// Load loads the risk levels json from S3 and returns a slice of bytes
+// Load loads the json object from S3 (path) into the given interface
 func (s Client) Load(i interface{}, path string) error {
 	if len(path) == 0 {
 		return errors.New("Path cannot be empty")
@@ -73,6 +74,42 @@ func (s Client) Load(i interface{}, path string) error {
 
 	if err := json.Unmarshal(o, i); err != nil {
 		return fmt.Errorf("Unable to unmarshal %T", i)
+	}
+
+	return nil
+}
+
+// PutObject takes a byte array and saves it in S3 with the given key name
+func (s Client) PutObject(object []byte, key string, ct string) error {
+	input := &s3.PutObjectInput{
+		Bucket:      aws.String(s.Bucket),
+		Key:         aws.String(key),
+		Body:        bytes.NewReader(object),
+		ContentType: aws.String(ct),
+	}
+
+	if _, err := s.Service.PutObject(input); err != nil {
+		log.Println(err.Error(), input)
+		return err
+	}
+
+	return nil
+}
+
+// Save saves an object to S3 (path) as JSON
+func (s Client) Save(i interface{}, path string) error {
+	if len(path) == 0 {
+		return errors.New("Path cannot be empty")
+	}
+
+	log.Printf("Saving %T to %s", i, path)
+	j, err := json.MarshalIndent(i, "", "  ")
+	if err != nil {
+		return fmt.Errorf("Unable to marshal %T", i)
+	}
+
+	if err = s.PutObject(j, path, "application/json"); err != nil {
+		return errors.New("Unable to put object into S3")
 	}
 
 	return nil
