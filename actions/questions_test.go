2597a40b0de2f180397cebd719a904857c037312
Tenyo Grozev <tenyo.grozev@yale.edu>
2018-10-18 10:08:42 -0400

    fixup

--- actions/questions_test.go
+++ actions/questions_test.go
@@ -4,7 +4,7 @@ func (as *ActionSuite) Test_QuestionsGet() {
 	wantBody := `{"questions":{"1":{"text":"Do you have HIPAA data?","long_text":"","answers":{"a":{"text":"Yes","datatypes":null},"b":{"text":"No","datatypes":null}}}},"version":"2.0","risklevels_version":"1.0","updated":""}`
 	wantCode := 200
 
-	got := as.HTML("/v1/proctor/test/questions").Get()
+	got := as.JSON("/v1/proctor/test/questions").Get()
 	as.Equal(wantCode, got.Code)
 	as.Contains(got.Body.String(), wantBody)
 }
--- actions/risklevels_test.go
+++ actions/risklevels_test.go
@@ -4,7 +4,7 @@ func (as *ActionSuite) Test_RiskLevelsGet() {
 	wantBody := `{"risklevels":[{"text":"high","score":30,"datatypes":["HIPAA","PCI","SSN"]},{"text":"moderate","score":20,"datatypes":["FERPA"]},{"text":"low","score":0,"datatypes":[]}],"version":"1.0","updated":""}`
 	wantCode := 200
 
-	got := as.HTML("/v1/proctor/risklevels").Get()
+	got := as.JSON("/v1/proctor/risklevels").Get()
 	as.Equal(wantCode, got.Code)
 	as.Contains(got.Body.String(), wantBody)
 }
